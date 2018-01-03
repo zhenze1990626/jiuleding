6e9fcc380e4920fe716d9d510d56e56ad37bb479
zhenze1990626 <35018755+zhenze1990626@users.noreply.github.com>
2018-01-03 11:00:20 +0800

    小程序
    电商平台酒乐订

---
+++ data/car-data.js
@@ -0,0 +1,22 @@
+var localdata = [
+  {
+    name: "洋河蓝色经典",
+    price: 10,
+    style: "商务定制",
+    image1:"/images/car/car_jiu.png",
+    postId: 0,
+    selected:false
+  },
+  {
+    name: "洋河蓝色经典",
+    price:100,
+    style: "商务定制",
+    image1: "/images/car/car_jiu.png",
+    postId: 0,
+    selected:false
+  },
+
+]
+module.exports = {
+  postList: localdata
+}
---
+++ data/welcome-data.js
@@ -0,0 +1,41 @@
+var localdata = [
+  {
+    post_img: "/images/welcome/shou.png",
+    post2_img: "/images/welcome/shou-he.png",
+    post3_img: "/images/welcome/daji.png",
+    post4_img: "/images/welcome/bai.png",
+    title: "五粮液寿宴定制酒",
+    title2:"￥188/瓶",
+    title3: "五粮液寿宴定制酒",
+    title4: "￥150/瓶",
+    postId: 0,
+    postId2:1
+  },
+  {
+    post_img: "/images/welcome/shou.png",
+    post2_img: "/images/welcome/shou-he.png",
+    post3_img: "/images/welcome/daji.png",
+    post4_img: "/images/welcome/bai.png",
+    title: "五粮液寿宴定制酒2",
+    title2: "￥130/瓶",
+    title3: "五粮液寿宴定制酒",
+    title4: "￥180/瓶",
+    postId: 2,
+    postId2: 3
+  },
+  {
+    post_img: "/images/welcome/shou.png",
+    post2_img: "/images/welcome/shou-he.png",
+    post3_img: "/images/welcome/daji.png",
+    post4_img: "/images/welcome/bai.png",
+    title: "五粮液寿宴定制酒2",
+    title2: "￥130/瓶",
+    title3: "五粮液寿宴定制酒",
+    title4: "￥180/瓶",
+    postId: 4,
+    postId2: 5
+  }
+]
+module.exports = {
+  postList: localdata
+}
